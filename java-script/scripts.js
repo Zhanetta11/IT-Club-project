de793bf57b03cb3ca27d1a90097ec6c8ab5ed9ed
Zhanetta11 <tynymbekova1112@gmail.com>
2024-03-22 07:20:12 +0600

    Add java-script, css and html for the "shop-icon" in the header of each page

--- java-script/scripts.js
+++ java-script/scripts.js
@@ -52,4 +52,27 @@ document.addEventListener('DOMContentLoaded', function () {
   
     showCurrentImage();
 });
-  
+
+
+// shop-icon
+var modal = document.getElementById('myModal');
+var btn = document.getElementById('cartBtn');
+
+btn.onclick = function(event) {
+  event.preventDefault(); 
+  modal.style.display = "block";
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+if(span) {
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
